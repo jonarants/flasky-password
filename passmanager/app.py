672f63b1e48da32ef4fefa4704b820b80995c4b8
Jonathan Vazquez Duran <jonathan.vazquezd@outlook.es>
2025-07-28 23:48:25 -0600

    reactivando salt

--- passmanager/app.py
+++ passmanager/app.py
@@ -29,7 +29,7 @@ def get_db_config():
     }
 
 def hash_password(password):
-    return bcrypt.generate_password_hash(password, 20).decode('utf-8')
+    return bcrypt.generate_password_hash(password, 12).decode('utf-8')
 
 def load_or_create_salt(call_type, user):
     config = get_db_config()
@@ -40,7 +40,7 @@ def load_or_create_salt(call_type, user):
           salt = os.urandom(16)
           salt_result = salt
       elif call_type == "r": # LA R O W yo las defino, así que no puede haber error de usuario
-          cursor.execute('SELECT encryption_salt FROM users WHERE user = %s',(user))
+          cursor.execute('SELECT encryption_salt FROM users WHERE user = %s',(user,))
           salt_result = cursor.fetchone()
           salt_result = salt_result[0]
     except Exception as e:
@@ -55,7 +55,7 @@ def get_key (password, salt):
         algorithm=hashes.SHA256(),
         length=32,
         salt=salt,
-        iterations=600_000,
+        iterations=200_000,
         backend=default_backend()
     )
     return base64.urlsafe_b64encode(kdf.derive(password.encode()))
@@ -65,7 +65,7 @@ def register():
     return render_template('register.html')
 
 @app.route('/register_user', methods=['POST'])
-def logging_in():
+def register_user():
     user = request.form['user']
     password = request.form['password']
     two_factor_secret = "placeholder data"#request.form['two_factor_secret']
@@ -83,10 +83,11 @@ def logging_in():
         cursor.execute('INSERT INTO users (user,password,two_factor_secret,two_factor_enabled,encryption_salt) VALUES (%s,%s,%s,%s,%s)',(user,hashed_password,two_factor_secret,two_factor_enabled,salt))
         connection.commit()
         cursor.close()
-        return render_template ('resultado3.html' ,user=user)
+        mensaje = f"The {user} was created correctly"
+        return render_template ('result_data.html' ,mensaje=mensaje)
     except Exception as e:
-        mensaje = "Error al insertar a la base de datos:" + str(e)
-        return render_template('resultado2.html', mensaje=mensaje)
+        mensaje = f"Error al insertar a la base de datos:" + str(e)
+        return render_template('result_data_error.html', mensaje=mensaje)
 
 
 
@@ -96,7 +97,7 @@ def login():
 
 
 @app.route('/auth_validation', methods=['POST'])
-def logging_in_test():
+def auth_validation():
     user = request.form['user']
     password = request.form['password']
     salt = load_or_create_salt("r",user)
@@ -111,9 +112,9 @@ def logging_in_test():
         cursor.close()
         if user_record:
             if bcrypt.check_password_hash(user_record['password'], password): #Compara los hashes para la autenticacion
-                return render_template('result_data', mensaje= f"El usuario {user_record['user']} existe y su contraseña es valida")
+                return render_template('result_data.html', mensaje= f"El usuario {user_record['user']} existe y su contraseña es valida")
             else:
-                return render_template('result_data', mensaje= f"Usuario o contraseña invalidos")
+                return render_template('result_data.html', mensaje= f"Usuario o contraseña invalidos")
     except Exception as e:
         mensaje = "Usuario no existente:" + str(e)
         return render_template('result_data_error.html', mensaje=mensaje)
@@ -124,27 +125,39 @@ def home():
     return render_template('home.html')
 
 @app.route('/website_info')
-def index():
+def website_info():
     return render_template('website_info.html')
 
 @app.route('/capture_website_data', methods=['POST'])
 def capture_website_data():
     user = request.form['user']
     password = request.form['password']
-    websites = request.form['websites']
+    website = request.form['website']
     try:
         config = get_db_config()
         connection = mysql.connector.connect(**config)
         cursor = connection.cursor()
-        cursor.execute('INSERT INTO websites_info (websites,user,password) VALUES(%s,%s,%s)',(websites, user,password))
+        cursor.execute('INSERT INTO websites_info (website,user,password) VALUES(%s,%s,%s)',(website, user,password))
         connection.commit()
         cursor.close()
         connection.close()
-        return render_template('result_data.html',websites=websites,user=user,password=password)
+        mensaje=f"La información para el sitio {website} fue agregada al password manager"
+        return render_template('website_info_added.html',mensaje=mensaje)
     except Exception as e:
         mensaje="Error al insertar en la base de datos:" + str(e)
         return render_template('result data_error.html',mensaje=mensaje)
-        
+
+@app.route('/show_tables')
+def show_tables():
+    config = get_db_config()
+    connection = mysql.connector.connect(**config)
+    cursor = connection.cursor(dictionary=True) # Se obtiene la info en modo diccionario
+    cursor.execute("SELECT * FROM websites_info")
+    websites = cursor.fetchall() 
+    connection.close()
+    return render_template('tables.html', websites=websites)
+
+
 if __name__=='__main__':
     app.run(host='0.0.0.0', port=5000, debug=True)
 
