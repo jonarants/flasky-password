e2bf0490761d01bddba515b1c0a066ccc6ace9a8
Jonathan Vazquez Duran <jonathan.vazquezd@outlook.es>
2025-08-03 17:02:34 -0600

    Se refactorizan los procesos de hash y encriptado

--- passmanager/secrets/read_secrets.py
+++ passmanager/secrets/read_secrets.py
@@ -1,10 +1,12 @@
 import os
 class ReadSecrets:
+
      def __init__(self, secrets_path='/run/secrets'):
           self.secrets_path = secrets_path
           if not os.path.isdir(self.secrets_path):
                print(f"The secrets are non-existant in the docker structure, make sure you're running through docker")
-     def get_secret(self, secret_name: str) -> str:
+
+     def get_secret(self, secret_name):
           secret_file_path = os.path.join(self.secrets_path,secret_name)
           try:
                with open(secret_file_path, 'r') as f:
