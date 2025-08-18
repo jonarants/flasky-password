958e69159c3a5cb020640dce81b91b8c4179b095
Jonathan Vazquez Duran <jonathan.vazquezd@outlook.es>
2025-08-17 21:30:26 -0600

    Added aria-hidden

--- passmanager/secrets/read_secrets.py
+++ passmanager/secrets/read_secrets.py
@@ -4,7 +4,7 @@ class ReadSecrets:
      def __init__(self, secrets_path='/run/secrets'):
           self.secrets_path = secrets_path
           if not os.path.isdir(self.secrets_path):
-               print(f"The secrets are non-existant in the docker structure, make sure you're running through docker")
+               print("The secrets are non-existant in the docker structure, make sure you're running through docker")
 
      def get_secret(self, secret_name):
           secret_file_path = os.path.join(self.secrets_path,secret_name)
