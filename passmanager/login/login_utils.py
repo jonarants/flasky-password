89335271c6ae9bf61087a6014097dd39d96a1ce0
Jonathan Vazquez Duran <jonathan.vazquezd@outlook.es>
2025-08-18 21:34:41 -0600

    LoginUtils created

--- passmanager/login/login_utils.py
+++ passmanager/login/login_utils.py
@@ -1,22 +1,15 @@
-from db.db_utils import DBUtils
-from secrets.read_secrets import ReadSecrets
-import mysql.connector
+
 class LoginUtils:
 
-    def __init__(self, read_secrets):
-        if not isinstance(read_secrets, ReadSecrets):
-            raise TypeError("read secrets must be an instance of ReadSecrets.")
-        self.read_secrets = read_secrets
+    def __init__(self):
+        self.session={}
 
-    def user_auth_role(self, user_record, password):
+    def user_auth_role(self, user_record):
         if user_record['admin'] == 1:
             self.session['admin'] = True
         else:
             self.session['admin'] = False
 
         session = self.session['admin']
-
-
-
 
         return session
